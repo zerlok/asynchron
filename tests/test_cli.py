e69fa9881695db8352c8ff86ef8a3cc015907129
zerlok <denergytro@gmail.com>
2021-12-01 22:18:30 +0300

    WIP: tests upgraded, code gen test fails

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,25 @@
 import json
 import typing as t
+from dataclasses import dataclass, field
 from pathlib import Path
 
 from click.testing import CliRunner
-from pytest_cases import fixture, parametrize_with_cases
+from pytest_cases import case, fixture, parametrize_with_cases
 
 from asyncapi.cli import cli
+
+
+@dataclass(frozen=True)
+class CliInput:
+    args: t.Sequence[str] = field(default_factory=tuple)
+    env: t.Mapping[str, str] = field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class CliOutput:
+    exit_code: int = 0
+    stdout: str = ""
+    fs_changes: t.Mapping[Path, t.Sequence[str]] = field(default_factory=dict)
 
 
 @fixture()
@@ -23,53 +37,129 @@ def target_dir(tmpdir: str) -> Path:
     return Path(tmpdir)
 
 
+@fixture()
+def file_content_loader() -> t.Callable[[Path], t.Mapping[Path, t.Sequence[str]]]:
+    def walk_file_paths(root: Path) -> t.Iterable[Path]:
+        paths: t.List[Path] = [root]
+        while paths:
+            path = paths.pop(0)
+            if path.is_dir():
+                paths.extend(path.iterdir())
+
+            if path.is_file():
+                yield path
+
+    def load_dir(root: Path) -> t.Mapping[Path, t.Sequence[str]]:
+        results = {}
+
+        for path in walk_file_paths(root):
+            with path.open("r") as fd:
+                results[Path(*path.parts[len(root.parts):])] = fd.readlines()
+
+        return results
+
+    return load_dir
+
+
 class ConfigCases:
-    def case_config_001(self, data_dir: Path) -> t.Tuple[Path, t.Any, Path]:
-        with (data_dir / "config-001" / "final.json").open("r") as fd:
+    def case_config_001(
+            self,
+            data_dir: Path,
+            file_content_loader: t.Callable[[Path], t.Mapping[Path, t.Sequence[str]]],
+    ) -> t.Tuple[Path, t.Any, t.Mapping[Path, t.Sequence[str]]]:
+        root = data_dir / "config-001"
+
+        with (root / "final.json").open("r") as fd:
             config_json = json.load(fd)
 
-        return data_dir / "config-001" / "asyncapi.yaml", config_json, data_dir / "final_code"
+        return root / "asyncapi.yaml", config_json, file_content_loader(root / "final_code")
 
 
 # TODO: i want to do less copy-past for parametrizing tests with `ConfigCases`
 class CliCases:
     @parametrize_with_cases(("config_path", "config_json"), ConfigCases)
+    @case(tags=("read", "compact", "success", "has-output"))
     def case_read_config_in_compact_format(
             self,
             config_path: Path,
             config_json: t.Any,
-    ) -> t.Tuple[t.Mapping[str, t.Optional[str]], t.Sequence[t.Any], int, str]:
-        return {}, ("-f", str(config_path), "get",), 0, \
-               json.dumps(config_json, indent=None, separators=(",", ":"), sort_keys=True) + "\n"
+    ) -> t.Tuple[CliInput, CliOutput]:
+        return (
+            CliInput(
+                args=("-f", str(config_path), "get",),
+            ),
+            CliOutput(
+                stdout=json.dumps(config_json, indent=None, separators=(",", ":"), sort_keys=True),
+            ),
+        )
 
     @parametrize_with_cases(("config_path", "config_json"), ConfigCases)
+    @case(tags=("read", "pretty", "success", "has-output"))
     def case_read_config_in_pretty_format(
             self,
             config_path: Path,
             config_json: t.Any,
-    ) -> t.Tuple[t.Mapping[str, t.Optional[str]], t.Sequence[t.Any], int, str]:
-        return {}, ("-f", str(config_path), "get", "--pretty"), 0, \
-               json.dumps(config_json, indent=2, separators=(", ", ": "), sort_keys=True) + "\n"
+    ) -> t.Tuple[CliInput, CliOutput]:
+        return (
+            CliInput(
+                args=("-f", str(config_path), "get", "--pretty",),
+            ),
+            CliOutput(
+                stdout=json.dumps(config_json, indent=2, separators=(", ", ": "), sort_keys=True),
+            ),
+        )
 
-    @parametrize_with_cases(("config_path", "config_json", "expected_output_dir"), ConfigCases)
+    @parametrize_with_cases(("config_path", "config_json", "project_file_content"), ConfigCases)
+    @case(tags=("generate", "python-aio-pika", "success", "fs-changes",))
     def case_generate_python_aio_pika_app_code_from_config(
             self,
             config_path: Path,
             config_json: t.Any,
-            expected_output_dir: Path,
+            project_file_content: t.Mapping[Path, t.Sequence[str]],
             target_dir: Path,
-    ) -> t.Tuple[t.Mapping[str, t.Optional[str]], t.Sequence[t.Any], int, str]:
-        return {}, ("-f", str(config_path), "generate", "python-aio-pika", "-o", str(target_dir)), 0, ""
+    ) -> t.Tuple[CliInput, CliOutput]:
+        return (
+            CliInput(
+                args=("-f", str(config_path), "generate", "python-aio-pika", "-o", str(target_dir),),
+            ),
+            CliOutput(
+                fs_changes=project_file_content,
+            ),
+        )
 
 
-@parametrize_with_cases(("env", "args", "expected_exit_code", "expected_output",), (CliCases,))
-def test_cli(
+@parametrize_with_cases(("input", "output",), (CliCases,), )
+def test_cli_exit_code(
         cli_runner: CliRunner,
-        env: t.Mapping[str, t.Optional[str]],
-        args: t.Sequence[t.Any],
-        expected_exit_code: int,
-        expected_output: str,
+        input: CliInput,
+        output: CliOutput,
 ) -> None:
-    result = cli_runner.invoke(cli, args=args, env=env)
+    result = cli_runner.invoke(cli, args=input.args, env=input.env)
 
-    assert (result.exit_code, result.stdout) == (expected_exit_code, expected_output)
+    assert result.exit_code == output.exit_code
+
+
+@parametrize_with_cases(("input", "output",), (CliCases,), )
+def test_cli_output(
+        cli_runner: CliRunner,
+        input: CliInput,
+        output: CliOutput,
+) -> None:
+    result = cli_runner.invoke(cli, args=input.args, env=input.env)
+
+    assert result.stdout[:-1] == output.stdout
+
+
+@parametrize_with_cases(("input", "output",), (CliCases,), )
+def test_cli_fs_changes(
+        cli_runner: CliRunner,
+        input: CliInput,
+        output: CliOutput,
+        target_dir: Path,
+        file_content_loader: t.Callable[[Path], t.Mapping[Path, t.Sequence[str]]],
+) -> None:
+    assert not file_content_loader(target_dir)
+
+    cli_runner.invoke(cli, args=input.args, env=input.env)
+
+    assert file_content_loader(target_dir) == output.fs_changes
